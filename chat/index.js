6c45409c0c3bf62266505a05589d5496ff5dead6
Ellaylone <ellaylone@yandex-team.ru>
2016-12-30 22:18:39 +0300

    improvements

--- chat/index.js
+++ chat/index.js
@@ -4,8 +4,7 @@ const app = require('express')(),
       path = require('path'),
       http = require('http').Server(app),
       io = require('socket.io')(http),
-      uuid = require('node-uuid'),
-      Room = require('./room.js');
+      uuid = require('node-uuid');
 
 let people = {},
     rooms = {},
@@ -19,24 +18,43 @@ app.get('/index.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.css'));
 });
 
+function getTime () {
+    const now = new Date(),
+          hours = ('00' + now.getHours()).substr(-2,2),
+          minutes = ('00' + now.getMinutes()).substr(-2,2);
+
+    return `[${hours}:${minutes}]`;
+}
+
 io.on('connection', socket => {
     socket.on('login', name => {
         const userList = [];
 
         people[socket.id] = {
             "name" : name,
-            "room" : 'ALL'
+            "room" : 0,
+            "uuid" : uuid.v4()
         };
 
-        socket.join('ALL');
-        socket.emit('login', 'ALL');
+        socket.join(0);
+        socket.emit('login', 0);
 
-        socket.broadcast.to(people[socket.id].room).emit('chat message', `${people[socket.id].name} has joined.`);
-        io.in(people[socket.id].room).emit('user join', people[socket.id].name);
+        socket.broadcast.to(people[socket.id].room).emit('chat message', {
+            author: 'System',
+            timestamp: getTime(),
+            text: `${people[socket.id].name} has joined.`
+        });
+        io.in(people[socket.id].room).emit('user join', {
+            name: people[socket.id].name,
+            uuid: people[socket.id].uuid
+        });
 
         for (let user in people) {
             if (people[user] && typeof people[user].name !== 'undefined') {
-                userList.push(people[user].name);
+                userList.push({
+                    name: people[user].name,
+                    uuid: people[user].uuid
+                });
             }
 
             continue;
@@ -47,42 +65,39 @@ io.on('connection', socket => {
         clients.push(socket);
     });
 
-    socket.on('typingMessage', (user) => {
+    socket.on('get userinfo', () => {
+        socket.emit('userinfo', people[socket.id]);
+    });
+
+    socket.on('join private', (room) => {
+
+    });
+
+    socket.on('typing message', (user) => {
         if(people[socket.id]) {
-            socket.broadcast.to(people[socket.id].room).emit('typingMessage', {
+            socket.broadcast.to(people[socket.id].room).emit('typing message', {
                 name: people[socket.id].name,
                 isTyping: user.isTyping
             });
         }
     });
 
-    socket.on('createRoom', name => {
-        if (people[client.id].room === null) {
-            var id = uuid.v4();
-            var room = new Room(name, id, client.id);
-            rooms[id] = room;
-            io.sockets.emit("roomList", {rooms: rooms}); //update the list of rooms on the frontend
-            client.room = name; //name the room
-            client.join(client.room); //auto-join the creator to the room
-            room.addPerson(client.id); //also add the person to the room object
-            people[client.id].room = id;
-        } else {
-            socket.sockets.emit("update", "You have already created a room.");
-        }
-    });
-
     socket.on('chat message', msg => {
-        const now = new Date(),
-              hours = ('00' + now.getHours()).substr(-2,2),
-              minutes = ('00' + now.getMinutes()).substr(-2,2);
-
-        msg ? socket.broadcast.to(people[socket.id].room).emit('chat message', `[${hours}:${minutes}] ${people[socket.id].name}: ${msg}`) : '';
+        msg ? socket.broadcast.to(people[socket.id].room).emit('chat message', {
+            author: people[socket.id].name,
+            timestamp: getTime(),
+            text: msg
+        }) : '';
     });
 
     socket.on('disconnect', () => {
         if (people[socket.id]) {
             socket.broadcast.to(people[socket.id].room).emit('user leave', people[socket.id].name);
-            io.emit('chat message', `${people[socket.id].name} has left.`);
+            io.emit('chat message', {
+                author: 'System',
+                timestamp: getTime(),
+                text: `${people[socket.id].name} has left.`
+            });
             people[socket.id] = null;
         }
     });
